a7c8655dc8ea071f655fcd605882089650a974cf
raylax <raylax@inurl.org>
2020-01-14 10:29:06 +0800

    add CI

--- install.rb
+++ install.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
 IMAGES_DIR = "images"
-DOCKER_HUB_USER = "raylax"
+DOCKER_HUB_USER = ENV['DOCKER_USERNAME']
 
 def process_file(file)
+  printf "[*] process file [%s]\n", file
   File.open(IMAGES_DIR + "/" + file, "r") do |f|
     f.each_line do |line|
       process_line file, line.strip
@@ -19,11 +20,23 @@ end
 
 def install_images(prefix, image)
   local_image = DOCKER_HUB_USER + "/" + image
-  puts "[*] pulling image " + local_image
+  printf "[*] pulling cached image [%s]\n", local_image
   pulled = system "docker", "pull", local_image
   if pulled
-    puts "[*] "
+    printf "[*] local image already exists, skipped\n"
+    return
   end
+  remote_image = prefix + "/" + image
+  printf "[*] pulling remote image [%s]\n", remote_image
+  pulled = system "docker", "pull", remote_image
+  unless pulled
+    printf "[*] remote image [%s] not found, skipped", remote_image
+    return
+  end
+  printf "[*] tag remote image [%s] to cached image [%s]", remote_image, local_image
+  system "docker", "tag", remote_image, local_image
+  printf "[*] pushing [%s] to docker hub"
+  system "docker", "push", local_image
 end
 
 Dir.foreach(IMAGES_DIR) do |file|
